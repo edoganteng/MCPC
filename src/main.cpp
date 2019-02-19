52a440500111a6bac035a3237aaa0083ef4a8d3b
neo3587 <neo3587@users.noreply.github.com>
2019-02-19 20:36:34 +0100

    fake stake fix + enforcement attempt

--- src/main.cpp
+++ src/main.cpp
@@ -254,6 +254,81 @@ struct CBlockReject {
     uint256 hashBlock;
 };
 
+//FAKE STAKE FIX
+class CNodeBlocks
+ {
+ public:
+     CNodeBlocks():
+             maxSize(0),
+             maxAvg(0)
+     {
+         maxSize = GetArg("-blockspamfiltermaxsize", 100);
+         maxAvg = GetArg("-blockspamfiltermaxavg", DEFAULT_BLOCK_SPAM_FILTER_MAX_AVG);
+     }
+
+       bool onBlockReceived(int nHeight) {
+         if(nHeight > 0 && maxSize && maxAvg) {
+             addPoint(nHeight);
+             return true;
+         }
+         return false;
+     }
+
+       bool updateState(CValidationState& state, bool ret)
+     {
+         // No Blocks
+         size_t size = points.size();
+         if(size == 0)
+             return ret;
+
+           // Compute the number of the received blocks
+         size_t nBlocks = 0;
+         for (auto point : points) {
+             nBlocks += point.second;
+         }
+
+           // Compute the average value per height
+         double nAvgValue = (double)nBlocks / size;
+
+           // Ban the node if try to spam
+         bool banNode = (nAvgValue >= 1.5 * maxAvg && size >= maxAvg) ||
+                        (nAvgValue >= maxAvg && nBlocks >= maxSize) ||
+                        (nBlocks >= maxSize * 3);
+
+           if (banNode) {
+             // Clear the points and ban the node
+             points.clear();
+             return state.DoS(100, error("block-spam ban node for sending spam"));
+         }
+
+           return ret;
+     }
+
+   private:
+     void addPoint(int height)
+     {
+         // Remove the last element in the list
+         if(points.size() == maxSize)
+         {
+             points.erase(points.begin());
+         }
+
+           // Add the point to the list
+         int occurrence = 0;
+         auto mi = points.find(height);
+         if (mi != points.end())
+             occurrence = (*mi).second;
+         occurrence++;
+         points[height] = occurrence;
+     }
+
+   private:
+     std::map<int,int> points;
+     size_t maxSize;
+     size_t maxAvg;
+ };
+
+
 /**
  * Maintain validation-specific state about nodes, protected by cs_main, instead
  * by CNode's own locks. This simplifies asynchronous operation, where
@@ -287,6 +362,8 @@ struct CNodeState {
     int nBlocksInFlight;
     //! Whether we consider this a preferred download peer.
     bool fPreferredDownload;
+	
+	CNodeBlocks nodeBlocks;
 
     CNodeState()
     {
@@ -3631,7 +3708,7 @@ bool AcceptBlockHeader(const CBlock& block, CValidationState& state, CBlockIndex
     return true;
 }
 
-bool AcceptBlock(CBlock& block, CValidationState& state, CBlockIndex** ppindex, CDiskBlockPos* dbp)
+bool AcceptBlock(CBlock& block, CValidationState& state, CBlockIndex** ppindex, CDiskBlockPos* dbp,  bool fAlreadyCheckedBlock)
 {
     AssertLockHeld(cs_main);
 
@@ -3645,7 +3722,8 @@ bool AcceptBlock(CBlock& block, CValidationState& state, CBlockIndex** ppindex, 
             return state.DoS(0, error("%s : prev block %s not found", __func__, block.hashPrevBlock.ToString().c_str()), 0, "bad-prevblk");
         pindexPrev = (*mi).second;
         if (pindexPrev->nStatus & BLOCK_FAILED_MASK)
-            return state.DoS(100, error("%s : prev block invalid", __func__), REJECT_INVALID, "bad-prevblk");
+            return state.DoS(100, error("%s : prev block %s is invalid, unable to add block %s", __func__, block.hashPrevBlock.GetHex(), block.GetHash().GetHex()),
+                             REJECT_INVALID, "bad-prevblk");
     }
 
     if (block.GetHash() != Params().HashGenesisBlock() && !CheckWork(block, pindexPrev))
@@ -3660,7 +3738,7 @@ bool AcceptBlock(CBlock& block, CValidationState& state, CBlockIndex** ppindex, 
         return true;
     }
 
-    if ((!CheckBlock(block, state)) || !ContextualCheckBlock(block, state, pindex->pprev)) {
+    if ((!fAlreadyCheckedBlock && !CheckBlock(block, state)) || !ContextualCheckBlock(block, state, pindex->pprev)) {
         if (state.IsInvalid() && !state.CorruptionPossible()) {
             pindex->nStatus |= BLOCK_FAILED_VALID;
             setDirtyBlockIndex.insert(pindex);
@@ -3775,28 +3853,40 @@ bool ProcessNewBlock(CValidationState& state, CNode* pfrom, CBlock* pblock, CDis
         }
     }
 
-    while (true) {
-        TRY_LOCK(cs_main, lockMain);
-        if (!lockMain) {
-            MilliSleep(50);
-            continue;
-        }
+    {
+        LOCK(cs_main);   // Replaces the former TRY_LOCK loop because busy waiting wastes too much resources
 
-        MarkBlockAsReceived(pblock->GetHash());
+        MarkBlockAsReceived (pblock->GetHash ());
         if (!checked) {
-            return error("%s : CheckBlock FAILED", __func__);
+            return error ("%s : CheckBlock FAILED for block %s", __func__, pblock->GetHash().GetHex());
         }
+       if (pfrom && GetBoolArg("-blockspamfilter", DEFAULT_BLOCK_SPAM_FILTER)) {
+             CNodeState *nodestate = State(pfrom->GetId());
+             BlockMap::iterator mi = mapBlockIndex.find(pblock->hashPrevBlock);
+             // we already checked this isn't the end
+             nodestate->nodeBlocks.onBlockReceived(mi->second->nHeight);
+             bool nodeStatus = true;
+             // UpdateState will return false if the node is attacking us or update the score and return true.
+             nodeStatus = nodestate->nodeBlocks.updateState(state, nodeStatus);
+             int nDoS = 0;
+             if (state.IsInvalid(nDoS)) {
+                 if (nDoS > 0)
+                     Misbehaving(pfrom->GetId(), nDoS);
+                 nodeStatus = false;
+             }
+             if(!nodeStatus)
+                 return error("%s : AcceptBlock FAILED - block spam protection", __func__);
+         }
 
         // Store to disk
         CBlockIndex* pindex = NULL;
-        bool ret = AcceptBlock(*pblock, state, &pindex, dbp);
+        bool ret = AcceptBlock (*pblock, state, &pindex, dbp, checked);
         if (pindex && pfrom) {
-            mapBlockSource[pindex->GetBlockHash()] = pfrom->GetId();
+            mapBlockSource[pindex->GetBlockHash ()] = pfrom->GetId ();
         }
         CheckBlockIndex();
         if (!ret)
-            return error("%s : AcceptBlock FAILED", __func__);
-        break;
+            return error ("%s : AcceptBlock FAILED", __func__);
     }
 
     if (!ActivateBestChain(state, pblock))
